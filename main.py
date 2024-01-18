620f1dbf06a6ab801616760402d35c1ea09b14ec
KurmanalievSamat <pluxury.sport03@mail.ru>
2024-01-18 19:43:41 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# for i in range(1, 30):
+#     if i % 2 == 0:
+#         print(i)
+
+my_string = 'samprime'
+x = 0
+
+for i in my_string:
+    x = x + 1
+    print(my_string[0:x])
+for i in my_string:
+    x = x - 1
+    print(my_string[0:x])
